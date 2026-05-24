ef4ea1f1c703eaff5204323065744d8be98b52bb
Rafael Moraes <rafaeljrmoraes@outlook.com>
2026-05-24 15:46:54 -0300

    first commit

---
+++ src/main/java/com/ex_rafah_co/navigation_flow_vd/NavigationFlowVdApplication.java
@@ -0,0 +1,13 @@
+package com.ex_rafah_co.navigation_flow_vd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavigationFlowVdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavigationFlowVdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ex_rafah_co/navigation_flow_vd/NavigationFlowVdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex_rafah_co.navigation_flow_vd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavigationFlowVdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
